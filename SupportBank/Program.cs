8332cf713231558f7dc1d52fa5a0f96806e168e9
Robin Leach <Robin.Leach@softwire.com>
2018-07-11 10:18:40 +0100

    Blank project

---
+++ SupportBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SupportBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
