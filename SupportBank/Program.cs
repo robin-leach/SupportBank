532264ddfa7e65aef380d532d02252dfccdf8f87
Robin Leach <Robin.Leach@softwire.com>
2018-07-11 11:11:48 +0100

    Looks at individual transactions, and continued building the classes I may need

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -6,13 +6,24 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
-            string whole_file = System.IO.File.ReadAllText(@"C:\Work\Training\SupportBank\Transactions2014.csv");
+            string whole_file = System.IO.File.ReadAllText(@"C:\Work\Training\SupportBank\Transactions2014.csv"); 
+                          // Reads CSV file, stores it as one long string called whole_file
             whole_file = whole_file.Replace('\n', '\r');
+                          // Replaces '\n' in the new file by '\r'
             string[] lines = whole_file.Split(new char[] { '\r' },
-                StringSplitOptions.RemoveEmptyEntries);
-            int entries = lines.Length - 1;
+                 StringSplitOptions.RemoveEmptyEntries);
+                          // Splits the whole file into lines, wherever there is an instance of '\r', and removes any empty entries
+            int entries = lines.Length - 1;  // create a variable to know how many transactions we're dealing with
             Console.WriteLine(entries);
             Console.ReadLine();
+
+            for(int i = 1; i<= entries; i++)
+            {
+                string[] transaction = lines[i].Split(',');
+                Console.WriteLine(transaction[0]);
+                Console.ReadLine();
+            }
+
         }
     }
 
