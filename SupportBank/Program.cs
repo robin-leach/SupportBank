c2ecd7f5443e480d0ba530619f2da2af6def2929
Robin Leach <Robin.Leach@softwire.com>
2018-07-11 10:51:26 +0100

    reads CSV file, stores it as an array of rows. started making the class to record each transaction.

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -6,7 +6,21 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string whole_file = System.IO.File.ReadAllText(@"C:\Work\Training\SupportBank\Transactions2014.csv");
+            whole_file = whole_file.Replace('\n', '\r');
+            string[] lines = whole_file.Split(new char[] { '\r' },
+                StringSplitOptions.RemoveEmptyEntries);
+            int entries = lines.Length - 1;
+            Console.WriteLine(entries);
+            Console.ReadLine();
         }
+    }
+
+    public class transaction
+    {
+        public string Date;
+        public string From;
+        public string To;
+        public float Amount;
     }
 }
