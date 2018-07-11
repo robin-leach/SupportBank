6f31fc253ab78af1620d0a8465a0c33253f35216
Robin Leach <Robin.Leach@softwire.com>
2018-07-11 12:28:08 +0100

    added working List All feature

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -8,6 +8,26 @@ namespace SupportBank
 {
     class Program
     {
+        static void List_all(List<string> people, int entries, string[] lines)
+        {
+            foreach (string human in people)                       // Now we go through each person and find out how much they owe/are owed
+            {
+                double owed = 0;    // Start owed at 0
+                double owes = 0;    // Start owes at 0
+                for (int i = 1; i <= entries; i++)  // Go through each transaction
+                {
+                    string[] transaction = lines[i].Split(',');  // Again, split each transaction into pieces
+                    if (transaction[1] == human)    // If the person is the ower...
+                        owes += Convert.ToDouble(transaction[4]);    //... add it to owes
+                    if (transaction[2] == human)   // If the person is the owee (is that a word?)...
+                        owed += Convert.ToDouble(transaction[4]);    //... add it to owed
+                }
+                double total = owes - owed;
+                Console.WriteLine("{0} owes {1} and is owed {2}. In total, they owe {3}", human, owes, owed, total);   // List every person and their total owed/owes
+            }
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
             string whole_file = System.IO.File.ReadAllText(@"C:\Work\Training\SupportBank\Transactions2014.csv");
@@ -28,24 +48,25 @@ namespace SupportBank
             }
             people = people.Distinct().ToList();                  // Remove any duplicates
 
-            foreach(string human in people)                       // Now we go through each person and find out how much they owe/are owed
+            bool running = true;
+            while (running == true)
             {
-                double owed = 0;    // Start owed at 0
-                double owes = 0;    // Start owes at 0
-                for(int i=1; i<= entries; i++)  // Go through each transaction
-                {
-                    string[] transaction = lines[i].Split(',');  // Again, split each transaction into pieces
-                    if (transaction[1] == human)    // If the person is the ower...
-                        owes += Convert.ToDouble(transaction[4]);    //... add it to owes
-                    if (transaction[2] == human)   // If the person is the owee (is that a word?)...
-                        owed += Convert.ToDouble(transaction[4]);    //... add it to owed
-                }
-                Console.WriteLine("{0} owes {1} and is owed {2}", human, owes, owed);   // List every person and their total owed/owes
+                Console.WriteLine("Enter a command (List All or Exit): ");
+                string input = Console.ReadLine(); // Get user input for the command"
+                if (input == "List All")
+                    List_all(people, entries, lines);
+                else if (input == "Exit")
+                    running = false;
+                else
+                    Console.WriteLine("Invalid command, try again");
             }
-            Console.ReadLine();
+
+
         }
     }
 
+
+    
     public class Debt
     {
         public string Date;
