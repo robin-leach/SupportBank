fb4c9a21cc1bdc9cbcae664431fa29354a2c948d
Robin Leach <Robin.Leach@softwire.com>
2018-07-11 13:59:10 +0100

    moved from individual variables to classes

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -8,22 +8,31 @@ namespace SupportBank
 {
     class Program
     {
-        static void List_all(List<string> people, int entries, string[] lines)
+        public class Debt
         {
-            foreach (string human in people)                       // Now we go through each person and find out how much they owe/are owed
+            public string Date;
+            public string From;
+            public string To;
+            public string Narrative;
+            public double Amount;
+        }
+
+        static void List_all(List<string> people, int entries, string[] lines, Debt[] debts)
+        {
+            for(int n=0; n<people.Count; n++)                       // Now we go through each person and find out how much they owe/are owed
             {
+                string current_person = people[n];
                 double owed = 0;    // Start owed at 0
                 double owes = 0;    // Start owes at 0
-                for (int i = 1; i <= entries; i++)  // Go through each transaction
+                for (int i = 0; i < entries; i++)  // Go through each transaction
                 {
-                    string[] transaction = lines[i].Split(',');  // Again, split each transaction into pieces
-                    if (transaction[1] == human)    // If the person is the ower...
-                        owes += Convert.ToDouble(transaction[4]);    //... add it to owes
-                    if (transaction[2] == human)   // If the person is the owee (is that a word?)...
-                        owed += Convert.ToDouble(transaction[4]);    //... add it to owed
+                    if (debts[i].From == current_person)     // If the person is the ower...
+                        owes += debts[i].Amount;    //... add it to owes
+                    if (debts[i].To == current_person)   // If the person is the owee (is that a word?)...
+                        owed += debts[i].Amount;    //... add it to owed
                 }
                 double total = owes - owed;
-                Console.WriteLine("{0} owes {1} and is owed {2}. In total, they owe {3}", human, owes, owed, total);   // List every person and their total owed/owes
+                Console.WriteLine("{0} owes {1} and is owed {2}. In total, they owe {3}.", current_person, owes, owed, total);   // List every person and their total owed/owes
             }
             Console.ReadLine();
         }
@@ -39,14 +48,27 @@ namespace SupportBank
             // Splits the whole file into lines, wherever there is an instance of '\r', and removes any empty entries
             int entries = lines.Length - 1;  // create a variable to know how many transactions we're dealing with
 
+            Debt[] debts = new Debt[entries];        // Record in an array, debts, each transaction as an instance of a class
+
             List<string> people = new List<string>();    // Here we aim to just list everybody involved
             for (int i = 1; i <= entries; i++)  // Look at each transaction in turn
             {
-                string[] transaction = lines[i].Split(',');       // Split the transactions into their different components
+                string[] transaction = lines[i].Split(',', StringSplitOptions.RemoveEmptyEntries);       // Split the transactions into their different components
+
+                Debt temp = new Debt();
+                temp.Date = transaction[0];  
+                temp.From = transaction[1];
+                temp.To = transaction[2];
+                temp.Narrative = transaction[3];
+                temp.Amount = Convert.ToDouble(transaction[4]);
+                debts[i - 1] = temp;
+
                 people.Add(transaction[1]);                       // Add whoever owes to the list
                 people.Add(transaction[2]);                       // Add whoever is owed to the list
             }
             people = people.Distinct().ToList();                  // Remove any duplicates
+
+
 
             bool running = true;
             while (running == true)
@@ -54,7 +76,7 @@ namespace SupportBank
                 Console.WriteLine("Enter a command (List All or Exit): ");
                 string input = Console.ReadLine(); // Get user input for the command"
                 if (input == "List All")
-                    List_all(people, entries, lines);
+                    List_all(people, entries, lines, debts);
                 else if (input == "Exit")
                     running = false;
                 else
@@ -67,13 +89,7 @@ namespace SupportBank
 
 
     
-    public class Debt
-    {
-        public string Date;
-        public string From;
-        public string To;
-        public float Amount;
-    }
+
 
     public class Person
     {
